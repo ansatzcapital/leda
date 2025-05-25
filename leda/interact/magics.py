e3ead3165338e7b693f823d9b0c3ff072d76c8ac
yanovs <42386943+yanovs@users.noreply.github.com>
2025-05-25 10:51:44 -0400

    Fix loading magics on test

--- leda/interact/magics.py
+++ leda/interact/magics.py
@@ -6,12 +6,29 @@ See
 https://ipython.readthedocs.io/en/stable/config/custommagics.html.
 """
 
-from typing import Any
+from typing import Any, Callable, TypeVar
 
+import IPython.core
 import IPython.core.magic
 
 import leda.interact.core
 import leda.interact.func_gen
+
+T = TypeVar("T")
+
+
+def no_op(obj: T) -> T:
+    return obj
+
+
+try:
+    get_ipython()  # type: ignore
+
+    register_line_magic = IPython.core.magic.register_cell_magic
+    register_line_cell_magic = IPython.core.magic.register_line_cell_magic
+except NameError:
+    register_line_magic = no_op
+    register_line_cell_magic = no_op
 
 
 def _interact(line: str, cell: str) -> Any:
@@ -21,11 +38,11 @@ def _interact(line: str, cell: str) -> Any:
     return leda.interact.core.get_interact_mode().interact(func, **kwargs)
 
 
-@IPython.core.magic.register_line_cell_magic
+@register_line_cell_magic
 def interact(line: str, cell: str) -> Any:
     return _interact(line, cell)
 
 
-@IPython.core.magic.register_line_magic
+@register_line_magic
 def toc(_: str) -> Any:
     return "Table of contents will be placed here in static mode."
