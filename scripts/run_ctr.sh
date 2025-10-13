6b1e8e499c99fe29bf995e68791a2268561e0ccb
yanovs <42386943+yanovs@users.noreply.github.com>
2025-10-13 13:44:22 -0400

    Fix run ctr script

--- scripts/run_ctr.sh
+++ scripts/run_ctr.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
+set -ex
 
 docker run --rm --platform linux/amd64 \
     -v .:/app/src -v /app/src/.pixi \
     --entrypoint bash -w /app/src \
     ghcr.io/prefix-dev/pixi:0.54.2-jammy \
-    -c 'apt update && apt install -y git && \
-    $@'
+    -c "apt update && apt install -y git && $*"
